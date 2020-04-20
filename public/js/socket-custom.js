4fe807393da6aeca75adfac125a03f8b73a6e09e
Iván <ivannsilvero@gmail.com>
2020-04-20 18:47:15 -0300

    Primer commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,25 @@
+var socket = io();
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+//Escuchar información
+socket.on('disconnect', function() {
+    console.log('Perdimos conexión con el servidor');
+});
+
+//Enviar información
+socket.emit('enviarMensaje', {
+    usuario: 'Iván',
+    mensaje: 'Hola Mundo'
+}, function(resp) {
+    console.log('respuesta server: ', resp);
+});
+
+//Escuchar información
+socket.on('enviarMensaje', function(mensaje) {
+
+    console.log('Servidor: ', mensaje);
+
+});
